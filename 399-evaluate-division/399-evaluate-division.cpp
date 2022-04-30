dfbb0c10afe18b7d52d7f1dc1ae4353480233d20
Sweta Seth <61093320+Sweta040799@users.noreply.github.com>
2022-04-30 23:43:42 +0530

    Time: 10 ms (7.26%), Space: 8.4 MB (52.62%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    unordered_map<string,unordered_map<string,double>> graph;
-    void dfs(string src,string des, double &cnt, double temp, unordered_map<string,int>& visit){
-        if(visit.count(src)){
+    unordered_map<string,unordered_map<string,double>> adj;
+    
+    void recur(unordered_map<string,int>& visit, string src, string dest, double& res, double temp){
+        if(visit[src]){
             return;
         }
-        
-        if(src == des){
-            cnt = temp;
+        if(src == dest){
+            res = temp;
             return;
         }
         visit[src] = 1;
-        for(auto node : graph[src]){
-           dfs(node.first,des,cnt,temp*node.second,visit); 
+        for(auto& it : adj[src]){
+            recur(visit,it.first,dest,res,temp * it.second);
         }
     }
+    
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        
-        
-        for(int i=0;i<equations.size();i++){
-            graph[equations[i][0]].insert({equations[i][1],values[i]});
-            graph[equations[i][1]].insert({equations[i][0],1.0/values[i]});
+        for(int i = 0; i < equations.size() ; i++){
+            adj[equations[i][0]].insert({equations[i][1],values[i]});
+            adj[equations[i][1]].insert({equations[i][0],1.0/values[i]});
         }
-        
-        int n = queries.size();
-        
         vector<double> ans;
-        for(auto it: queries){
-            string source = it[0];
-            string destination = it[1];
+        for(auto& it : queries){
+            string src = it[0];
+            string dest = it[1];
             unordered_map<string,int> visit;
-            
-            double count = 1.0;
-            double temp = -1.0;
-            
-            if(graph.find(source) == graph.end())
+             if(adj.find(src) == adj.end())
                  ans.push_back(-1.00000);
-            
             else{
-            dfs(source,destination,count,temp,visit);
-            ans.push_back(-count);
+                double res = 1.00, temp = -1.00;
+                recur(visit,src,dest,res,temp);
+                ans.push_back(-res);
             }
         }
         return ans;
