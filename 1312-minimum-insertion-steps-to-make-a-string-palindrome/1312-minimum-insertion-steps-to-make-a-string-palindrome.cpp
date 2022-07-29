7a48155985901a8ff821191b47febc4dfd3f0acc
Sweta Seth <61093320+Sweta040799@users.noreply.github.com>
2022-07-30 01:34:46 +0530

    Time: 80 ms (48.38%), Space: 27.2 MB (26.02%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    
-//     int recur(int i, int j, string s, int n){
-//         if(i >= j)
-//             return 0;
-        
-//         if(dp[i][j] != -1)
-//             return dp[i][j];
-        
-//         if(s[i] == s[j])
-//             return recur(i + 1, j - 1, s, n);
-        
-//         return dp[i][j] = min(1 + recur(i + 1, j, s, n), 1 + recur(i, j - 1, s, n));
-//     }
+
     int minInsertions(string s) {
         
         int n = s.size();
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
         
         for(int i = 1; i < n + 1; i++){
             for(int j = 1; j < n + 1; j++){
                 
-                if(s[i - 1] == s2[j - 1])
+                if(s[i - 1] == s[n - j])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
