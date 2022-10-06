2a7b22fd233f0e7ff0d3a7f091aa2fb2cd40bc26
Sweta Seth <61093320+Sweta040799@users.noreply.github.com>
2022-10-06 21:59:42 +0530

    Time: 25 ms (18.34%), Space: 9.9 MB (67.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    
+    int dp[201][201];
+    int Recur(vector<vector<int>>& grid, int r, int c){
+        if(r == 0 && c == 0) return grid[r][c];
+        if(r < 0 || c < 0) return 100000000;
+        
+        if(dp[r][c] != -1) return dp[r][c];
+        
+        int left = grid[r][c] + Recur(grid, r, c - 1);
+        int top = grid[r][c] + Recur(grid, r - 1, c);
+        
+        return dp[r][c] = min(left, top);
+    }
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        // vector<vector<int>> res(m,vector<int>(n,0));
-        // res[m-1][n-1] = grid[m-1][n-1];
-        for(int i = m - 1 ; i >= 0 ; i--) {
-            for(int j = n - 1 ; j >= 0 ; j--) {
-                int temp = INT_MAX;
-                if(j + 1 < n) temp = min(temp,grid[i][j+1]);
-                if(i + 1 < m) temp = min(temp,grid[i+1][j]);
-                grid[i][j] += temp == INT_MAX ? 0 : temp;
-                // cout<<i<<" "<<j<<" "<<grid[i][j]<<'\n';
-            }
-        }
-        return grid[0][0];
+        memset(dp, -1, sizeof(dp));
+        return Recur(grid, grid.size() - 1, grid[0].size() - 1);
     }
 };
